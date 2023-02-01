26d2b5695170c247aef4944048b9b2e03de730f9
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-02-01 04:08:34 +0300

    bitti

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let countries="";
+let cities="";
 let selectedCountry="";
-const select= document.querySelector(".form-select");
+let selectedCity="";
+
+const countrySelect= document.querySelector(".country-select");
+const weatherInfo=document.querySelector(".weather-info")
+const bok=document.querySelector(".bok")
+
 window.onload = function () {
     getCountriesDataFromRestApi();
   };
@@ -25,26 +31,62 @@ console.log(error);
 
 const getCountryNames=(data)=>{
 countries=data;
-console.log( "ülke dataları", countries);
+// console.log( "ülke dataları", countries);
 countries.forEach((country) => {
   document.querySelector(".form-select").innerHTML += `
     <option value='${country.name.common}'>${country.name.common}</option> `;
   });
 }
 
-select.addEventListener("change",(e)=>{
+
+
+
+
+countrySelect.addEventListener("change",(e)=>{
     const countryName=e.target.value;
     if (countryName) {
         const country=countries.filter(country=>country.name.common===countryName);
         selectedCountry=country[0].name.common;
         renderCountry(country[0])
     }
+    weatherInfo.innerHTML=`
+    <select class="weather-select"></select>
+    <div class="weather">bok</div>
+    `
+    
+       fetch(`https://countriesnow.space/api/v0.1/countries/population/cities`)
+       .then(res=>res.json())
+       .then(data=>getCityNames(data))
+       
+const getCityNames=(data)=>{
+cities=data.data;
+  console.log( "şehir dataları", cities);
+  const countryName=e.target.value;
+  console.log(countryName);
+  if (countryName) {
+      const city=cities.filter(city=>city.country===countryName);
+
+
+     city.map((city)=>{
+      document.querySelector(".weather-select").innerHTML+=`
+      <option value='${city.city}'>${city.city}</option>
+      `
+      console.log(selectedCity);
+     })
+}} 
+document.querySelector(".weather-select").addEventListener("change",()=>{
+  getWeatherDataByCityName(selectedCity[0])
+
+})
+
+
+
 })
 
 
 const renderCountry=(selectedCountryData)=>{
   const countriesInfo=document.querySelector(".countries-info");
-  console.log(selectedCountryData);
+  // console.log(selectedCountryData);
   const{name:{common},currencies, capital, languages, maps:{googleMaps},population,flags:{svg},region,borders }=selectedCountryData;
   countriesInfo.innerHTML=`
   <div class="card shadow-lg" style="width: 22rem">
@@ -92,13 +134,13 @@ const renderCountry=(selectedCountryData)=>{
 
 }
 
-console.log("67. satır seçilmiş ülke", selectedCountry);
+
 
 
 //? Ülkelerin hava durumu bilgisini al
-const getWeatherDataByCityName= async(cityName)=>{
+const getWeatherDataByCityName= async(selectedCity)=>{
     const API_KEY="f4a43088e4b5cd3c7f24923585afd1d9"; 
-    const URL= `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric&?lang=tr`
+    const URL= `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${API_KEY}&units=metric&?lang=tr`
 try {
     const res= await fetch(URL);
     if (!res.ok) {
@@ -106,33 +148,31 @@ try {
     }
 const data=await res.json();
 // console.log("Ülke hava durumu datası", data);
-// rendersCountrysWeather(data);
+rendersCountrysWeather(data);
 } catch (error) {
 // renderErrors(error)
 }
 }
-// getWeatherDataByCityName("kayseri")
-getWeatherDataByCityName("izmir")
-getWeatherDataByCityName("istanbul")
-getWeatherDataByCityName("muğla")
-getWeatherDataByCityName("london")
+
+
 
 
 
 
 const rendersCountrysWeather=(cityData)=>{
-    const cities=document.querySelector(".cities");
+   
 const {name,main:{temp, feels_like,temp_max
 ,temp_min }, weather, wind:{deg, gust, speed},sys:{country}}=cityData;
 console.log(name, temp,feels_like,weather[0].id);
-cities.innerHTML=`
+
+bok.innerHTML=`
 <div class="city">
 <div class="city-name">${name}<sup>${country}</sup> </div>
 <div class="city-temp">${temp}<sup>&#8451</></div>
 <div class="city-icon"> <img src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png" alt="img"></div>
 <div> <figcaption>${weather[0].main}</figcaption></div>
 </div>
-`+cities.innerHTML
+`+bok.innerHTML
 }
 
 
